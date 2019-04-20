04c7e5a42d777fc08a2b90013c0fef0f0118cfce
yutenghui <1819228931@qq.com>
2019-04-20 17:13:03 +0800

    microservice-user

---
+++ microservice_user/src/main/java/com/bchd/user/UserApplocation.java
@@ -0,0 +1,19 @@
+package com.bchd.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: yth
+ * @Date: 2019-04-20 16:45
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplocation {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplocation.class, args);
+    }
+}

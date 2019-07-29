65955b0c3e3a2e504ca1d8741b51085f3c109dbd
yutenghui <1819228931@qq.com>
2019-07-29 21:29:38 +0800

    SpringCloud Bus

--- microservice_user/src/main/java/com/bchd/user/controller/UserController.java
+++ microservice_user/src/main/java/com/bchd/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.bchd.common.utils.JWTUtil;
 import com.bchd.user.entity.User;
 import com.bchd.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +23,15 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController {
     @Autowired
     private IUserService iUserService;
     @Autowired
     private JWTUtil jwtUtil;
+
+    @Value("${jwt.config.key}")
+    private String jwt;
 
     /**
      * 用户登录
@@ -46,15 +52,19 @@ public class UserController {
     }
 
     /**
-     *
-     * @param code
-     * @param user
+     * 用户注册
+     * @param code 短信验证码
+     * @param user 用户
      * @return
      */
     @PostMapping(value = "/register/{code}")
     public Result register(@PathVariable String code, @RequestBody User user){
         iUserService.addUser(user);
         return Result.success();
+    }
+    @PostMapping(value = "/test")
+    public Result test(){
+        return Result.success(jwt);
     }
 
 
